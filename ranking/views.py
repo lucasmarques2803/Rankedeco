0a454419e5d6625e0a73e7cf562f1c9f5e5fa5b4
lucasmarques2003 <lucas-marques2003@usp.br>
2023-11-24 11:33:34 -0300

    Conserta erro em views.py

--- ranking/views.py
+++ ranking/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def index(request):
     context = {}
-    return render(request, 'staticpages/index.html', context)
+    return render(request, 'ranking/index.html', context)
