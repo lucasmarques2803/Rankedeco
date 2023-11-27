fd3b41fc6f4733c4e95248f7afa146fd4a7d6620
pedroazeredo04 <pedro_azeredo@usp.br>
2023-11-27 17:12:05 -0300

    Adiciona modelos
    Co-authored-by: lucasmarques2803 <lucasmarques2803@users.noreply.github.com>

--- ranking/models.py
+++ ranking/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Bandeco(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    bandeco = models.ManyToManyField(Bandeco, through="Nota")
+
+class Nota(models.Model):
+    bandeco = models.ForeignKey(Bandeco, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    value = models.IntegerField(default=0)
+
+class Comentario(models.Model):
+    date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    bandeco = models.ForeignKey(Bandeco, on_delete=models.CASCADE)
+    nota = models.IntegerField(default=0)
